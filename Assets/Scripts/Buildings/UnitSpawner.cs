81f142339264d53c91097a7ce7ca0ddc4dda3c50
Lance Days <lancedays@gmail.com>
2022-09-23 15:18:05 -0400

    Initial Commit

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,41 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform unitSpawnPoint = null;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+
+for(var i = 0; i < 1000 ; i++) {
+        GameObject unitIsntance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
+        NetworkServer.Spawn(unitIsntance, connectionToClient);
+}
+
+
+    }
+
+
+
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left) { return; }
+        if(!hasAuthority) { return; }
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,32 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+
+
+    #region Server
+    #endregion
+
+    #region Client
+    [Client]
+    public void Select()
+    {
+        if(!hasAuthority) { return; }
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deslect()
+    {
+        if(!hasAuthority){ return; }
+        onDeselected?.Invoke();
+    }
+    #endregion
+}
